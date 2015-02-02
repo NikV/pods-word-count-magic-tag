3e24076127dc0f86448560a3c83e63681b3ff01b
NikV <techvoltz@gmail.com>
2015-02-01 21:42:59 -0600

    Initial Commit

---
+++ pods-magic-tag-word-count.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Post Word Count Magic Tag
+ */
+
+function pods_more_stuff_things_more($code, $template_name,  $pods) {
+
+
+		$content      = str_word_count( get_post_field( 'post_content', $pods->ID() ));
+		$code         = str_replace( '{@post_count}', $content, $code );
+
+		return $code;
+
+
+
+}
+add_filter('pods_templates_pre_template', 'pods_more_stuff_things_more', 10, 3);
